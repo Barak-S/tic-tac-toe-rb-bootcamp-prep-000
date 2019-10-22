f84db40d4a254e2d028a614db9ca2b933e5e8eca
Barak Saidoff <barak.saidoff@gmail.com>
2019-10-22 22:02:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,14 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,49 +17,43 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#coverts a user's place on the board to the index integer
 def input_to_index(user_input)
-  index = user_input.to_i
-  index -= 1
-  return index
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
 end
 
 def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn_count(board)
+  turn = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
     end
   end
+  return turn
+end
 
-  def on_board?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
-
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
-    return true
+def current_player(board)
+  #if the turn count is an even number, that means O just went, so the next/current player is X
+  num_turns = turn_count(board)
+  if num_turns % 2 == 0
+    player = "X"
   else
-    return false
+    player = "O"
   end
+  return player
 end
 
-#Takes a users num and the board, and places the X or O char in that position
-# def move(board, index, character = "X")
-#   board[index] = character
-#   return board
-# end
-
-def move(board, index, character)
-  board[index] = character
-  return board
-end
-
-#Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
 def turn(board)
   puts "Please choose a number 1-9:"
   user_input = gets.chomp
@@ -59,56 +64,6 @@ def turn(board)
     display_board(board)
   else
     turn(board)
-  end
-end
-
-def play(board)
-  until over?(board) == true
-    turn(board)
-  end
-
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
-    puts "Cats Game!"
-  end
-end
-
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-# Define your WIN_COMBINATIONS constant
-
-WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5],  # Middle row
-  [6,7,8],   #bottom row
-  
-  [6,3,0], #left column
-  [7,4,1],  # middlw column
-  [8,5,2],  #right column
-  
-  [0,4,8], #horizontal 1
-  [2,4,6]   #horizontal 2
-]
-
-def turn_count(board)
-  turns = 0
-  board.each do |space|
-    if space == "X" || space == "O"
-      turns += 1
-    end
-  end
-  return turns
-end
-
-def current_player(board)
-  turns = turn_count(board)
-  if turns % 2 == 0
-    return "X"
-  else
-    return "O"
   end
 end
 
@@ -144,14 +99,14 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) || draw?(board) || full?(board)
+  if won?(board) || draw?(board)
     return true
   else
     return false
   end
 end
 
-def winner(board)
+def winner (board)
   index = []
   index = won?(board)
   if index == false
@@ -159,9 +114,20 @@ def winner(board)
   else
     if board[index[0]] == "X"
       return "X"
-    else 
+    else
       return "O"
     end
   end
 end
 
+def play(board)
+  until over?(board) == true
+    turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+end
